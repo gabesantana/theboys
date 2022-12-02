93b4f419ef4ea5e394d5a19e1904a6cd956f9cab
Witalo Rebouças <tilowr@gmail.com>
2022-12-02 18:29:28 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -6,3 +6,8 @@
 	
 	// criar a conexão
 	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+	
+	// Teste de Conexão 	
+	if (!$conn) {
+    		printf("Can't connect to localhost. Error: %s\n", mysqli_connect_error());
+	}
